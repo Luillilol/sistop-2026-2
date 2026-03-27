f670ab3cbf0fe39ffce5ca07c37580c71f100a07
David <david@pc-bd-dda.fi.unam>
2026-03-26 22:14:41 -0600

    Creación de la clase alumno, aun sin logica en run

--- tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
+++ tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
@@ -4,7 +4,23 @@ import threading as th
 import random
 import time
 
+numero_sillas = 10
 
+
+
+class Alumno(th.Thread):
+	def __init__(self, id, semaforo, mutex):
+		super().__init__()
+		self.id = id
+		self.semaforo = semaforo
+		self.mutex = mutex
+		self.numero_dudas = random.randint(1, 10)
+
+	def run(self):
+		semaforo.acquire()
+		
+
+		semaforo.release()
 
 
 
